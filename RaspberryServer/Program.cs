02e42b500f73386f35c838c499c1449150c85064
GorudoYami <gorudoyami@gmail.com>
2021-09-09 21:05:18 +0200

    Add project files.

---
+++ RaspberryServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RaspberryServer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Testing...");
+            Server server = new("localhost", 6666);
+
+            Console.WriteLine("Starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+            Console.WriteLine("Stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
